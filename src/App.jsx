90fbb7b64a021e6383a8bf31628da01f2bffa8a3
taroosg <macchinetta.xyz@gmail.com>
2020-03-08 19:06:40 +0900

    update filter&sort

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,21 @@ import events from './data/events.json';
 
 const App = () => {
 
+  const compare = (a, b) => {
+    const datetimeA = a.datetime.toUpperCase();
+    const datetimeB = b.datetime.toUpperCase();
+    return (datetimeA > datetimeB) ? 1 : -1;
+  }
+
   return (
     <div className="App">
       <main className="App-main">
         {
           (events.length > 0)
-            ? events.map((x, index) => <Counter key={index} event={x} />)
+            ? events
+              .filter(x => new Date(x.datetime).getTime() > new Date().getTime())
+              .sort(compare)
+              .map((x, index) => <Counter key={index} event={x} />)
             : <p>404 Event Not Found...</p>
         }
       </main>
